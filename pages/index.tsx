a5fb451b5ee4f79c7b83afe87cb785f252c0dfb0
Elza Gazizova <elza396@yandex.ru>
2025-01-17 02:53:25 +0500

    feat: add ts, eslint and prettier

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
+import { Calendar } from "../src/Calendar/Calendar";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Календарь вакцинации</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className={styles.title}>
+          А это{" "}
+          <a href="https://cmpmos.ru/naseleniyu/vaktsinatsiya-detey-i-vzroslyh-v-tsmp/natsionalnyy-kalendar-privivok/">
+            Национальный календарь прививок
+          </a>
+          kjk
+        </h1>
+
+        <Calendar />
+      </main>
+    </div>
+  );
+}
